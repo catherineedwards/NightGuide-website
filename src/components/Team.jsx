5956d6f466bf835a1bccb13b6b07a36080c51d5c
catherineedwards <catherine@edwards-au.com>
2018-12-12 12:46:47 +1300

    Added team photo

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,61 +5,84 @@ import Emoji from "react-emoji-render";
 class Team extends Component {
   render() {
     return (
-      <div>
-        <h2 id="team">The team</h2>
+      <React.Fragment>
         <div>
-          <img
-            className="avatar"
-            alt="Taz"
-            src={process.env.PUBLIC_URL + "taz.png"}
-          />
-          <p>
-            Taz: Aquarius <Emoji text="♒" />, Rooster <Emoji text="🐓" />
+          <h2 id="team">The team</h2>
+          <div className="teamPhoto">
+            <img
+              className="img-fluid"
+              alt="The team posing in front of Te Papa"
+              src={process.env.PUBLIC_URL + "teamphoto.png"}
+            />
+          </div>
+          <div>
+            <p className="avatar">
+              <img
+                className="img-fluid"
+                alt="Taz"
+                src={process.env.PUBLIC_URL + "taz.png"}
+              />{" "}
+              <br />
+              Taz: Aquarius <Emoji text="♒" />, Rooster <Emoji text="🐓" />
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="avatar">
+            <img
+              className="img-fluid"
+              alt="Ollie"
+              src={process.env.PUBLIC_URL + "ollie.png"}
+            />{" "}
+            <br />
+            Ollie: Aquarius <Emoji text="♒" />, Dog <Emoji text="🐕" />
           </p>
         </div>
         <div>
-          <img
-            className="avatar"
-            alt="Ollie"
-            src={process.env.PUBLIC_URL + "ollie.png"}
-          />
-          <p>
-            Ollie: Aquarius <Emoji text="♒" />, Dog{" "}
+          <p className="avatar">
+            <img
+              className="img-fluid"
+              alt="Joan"
+              src={process.env.PUBLIC_URL + "joan.png"}
+            />{" "}
+            <br />
+            Joan: Sagittarius <Emoji text="♐" />, Horse <Emoji text="🐎" />
           </p>
         </div>
         <div>
-          <img
-            className="avatar"
-            alt="Joan"
-            src={process.env.PUBLIC_URL + "joan.png"}
-          />
-          <p>Joan: Sagittarius, Horse</p>
+          <p className="avatar">
+            <img
+              className="img-fluid"
+              alt="Sungmi"
+              src={process.env.PUBLIC_URL + "sungmi.png"}
+            />{" "}
+            <br />
+            Sungmi: Capricorn <Emoji text="♑" />, Tiger <Emoji text="🐅" />
+          </p>
         </div>
         <div>
-          <img
-            className="avatar"
-            alt="Sungmi"
-            src={process.env.PUBLIC_URL + "sungmi.png"}
-          />
-          <p>Sungmi: Capricorn, Tiger</p>
+          <p className="avatar">
+            <img
+              className="img-fluid"
+              alt="Kelly"
+              src={process.env.PUBLIC_URL + "kelly.png"}
+            />{" "}
+            <br />
+            Kelly: Libra <Emoji text="♎" />, Monkey <Emoji text="🐒" />
+          </p>
         </div>
         <div>
-          <img
-            className="avatar"
-            alt="Kelly"
-            src={process.env.PUBLIC_URL + "kelly.png"}
-          />
-          <p>Kelly: Libra, Monkey</p>
+          <p className="avatar">
+            <img
+              className="img-fluid"
+              alt="Cathy"
+              src={process.env.PUBLIC_URL + "cathy.jpg"}
+            />{" "}
+            <br />
+            Cathy: Leo <Emoji text="♌" />, Tiger <Emoji text="🐅" />
+          </p>
         </div>
-        <div>
-          <img
-            className="avatar"
-            alt="Cathy"
-            src={process.env.PUBLIC_URL + "cathy.jpg"}
-          />
-          <p>Cathy: Leo, Tiger</p>
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
