3dfd2c30098207b6ffd7f50b5300a81de590dc5e
catherineedwards <catherine@edwards-au.com>
2018-12-12 16:16:25 +1300

    Further styling

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -15,72 +15,83 @@ class Team extends Component {
               src={process.env.PUBLIC_URL + "teamphoto.png"}
             />
           </div>
-          <div>
-            <p className="avatar">
-              <img
-                className="img-fluid"
-                alt="Taz"
-                src={process.env.PUBLIC_URL + "taz.png"}
-              />{" "}
-              <br />
-              Taz: Aquarius <Emoji text="♒" />, Rooster <Emoji text="🐓" />
-            </p>
+
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                {" "}
+                <img
+                  className="rounded"
+                  alt="Taz"
+                  src={process.env.PUBLIC_URL + "taz.png"}
+                />
+                <p className="profile">
+                  Taz: Aquarius <Emoji text="♒" />, Rooster <Emoji text="🐓" />
+                </p>
+              </div>
+
+              <div class="col">
+                {" "}
+                <img
+                  className="rounded"
+                  alt="Ollie"
+                  src={process.env.PUBLIC_URL + "ollie.png"}
+                />
+                <p className="profile">
+                  Ollie: Aquarius <Emoji text="♒" />, Dog <Emoji text="🐕" />
+                </p>
+              </div>
+
+              <div class="col">
+                <img
+                  className="rounded"
+                  alt="Joan"
+                  src={process.env.PUBLIC_URL + "joan.png"}
+                />
+                <p className="profile">
+                  Joan: Sagittarius <Emoji text="♐" />, Horse{" "}
+                  <Emoji text="🐎" />
+                </p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <img
+                  className="rounded"
+                  alt="Sungmi"
+                  src={process.env.PUBLIC_URL + "sungmi.png"}
+                />
+                <p className="profile">
+                  Sungmi: Capricorn <Emoji text="♑" />, Tiger{" "}
+                  <Emoji text="🐅" />
+                </p>
+              </div>
+              <div class="col">
+                {" "}
+                <img
+                  className="rounded"
+                  alt="Kelly"
+                  src={process.env.PUBLIC_URL + "kelly.png"}
+                />
+                <p className="profile">
+                  Kelly: Libra <Emoji text="♎" />, Monkey <Emoji text="🐒" />
+                </p>
+              </div>
+              <div class="col">
+                <p>
+                  {" "}
+                  <img
+                    className="rounded"
+                    alt="Cathy"
+                    src={process.env.PUBLIC_URL + "cathy.jpg"}
+                  />{" "}
+                </p>
+                <p className="profile">
+                  Cathy: Leo <Emoji text="♌" />, Tiger <Emoji text="🐅" />
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
-          <p className="avatar">
-            <img
-              className="img-fluid"
-              alt="Ollie"
-              src={process.env.PUBLIC_URL + "ollie.png"}
-            />{" "}
-            <br />
-            Ollie: Aquarius <Emoji text="♒" />, Dog <Emoji text="🐕" />
-          </p>
-        </div>
-        <div>
-          <p className="avatar">
-            <img
-              className="img-fluid"
-              alt="Joan"
-              src={process.env.PUBLIC_URL + "joan.png"}
-            />{" "}
-            <br />
-            Joan: Sagittarius <Emoji text="♐" />, Horse <Emoji text="🐎" />
-          </p>
-        </div>
-        <div>
-          <p className="avatar">
-            <img
-              className="img-fluid"
-              alt="Sungmi"
-              src={process.env.PUBLIC_URL + "sungmi.png"}
-            />{" "}
-            <br />
-            Sungmi: Capricorn <Emoji text="♑" />, Tiger <Emoji text="🐅" />
-          </p>
-        </div>
-        <div>
-          <p className="avatar">
-            <img
-              className="img-fluid"
-              alt="Kelly"
-              src={process.env.PUBLIC_URL + "kelly.png"}
-            />{" "}
-            <br />
-            Kelly: Libra <Emoji text="♎" />, Monkey <Emoji text="🐒" />
-          </p>
-        </div>
-        <div>
-          <p className="avatar">
-            <img
-              className="img-fluid"
-              alt="Cathy"
-              src={process.env.PUBLIC_URL + "cathy.jpg"}
-            />{" "}
-            <br />
-            Cathy: Leo <Emoji text="♌" />, Tiger <Emoji text="🐅" />
-          </p>
         </div>
       </React.Fragment>
     );
